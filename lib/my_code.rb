7d70faabff9ffcd1b2e092447f37ce7f86725207
Joseph Trettevik <josephtrettevik@gmail.com>
2019-08-06 21:42:17 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,49 @@
-# Your Code Here
+def map(source_array)
+  new = []
+  i = 0
+  while i < source_array.length do
+    new.push( yield(source_array[i]) )
+    i += 1
+  end
+  return new
+end
+
+def reduce(source_array, starting_point = 0)
+  result = starting_point
+  source_array.length.times do |index|
+    result = yield(result, source_array[index])
+  end 
+  return result
+end 
+
+#reduce(source_array) do 
+ # |memo, n| 
+#  puts " B: Memo = #{memo}, n = #{n}"
+#  ret = memo || n
+#  puts " A: Memo = #{memo}, n = #{n}" 
+#  puts ret
+#  return ret
+#end
+
+#def reduce_to_all_true(source_array, starting_point = 0)
+# result = starting_point
+#  source_array.length.times do |index|
+#    if !source_array[index]
+#      result = false
+#      return result
+#    end
+#  end 
+#  return result
+#end 
+
+#def reduce_to_any_true(source_array, starting_point = 0)
+#  result = starting_point
+#  source_array.length.times do |index|
+#    if source_array[index]
+#      result = true
+#      return result
+#    end
+#  end
+#  return result
+#end
+
